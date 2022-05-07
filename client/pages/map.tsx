c9eb2e4a28bdd0b7b555edfede9d4bbc90f7ec57
Jacarder <jonandrewcarder@gmail.com>
2022-05-06 23:24:58 -0400

    Messing around with potential styling

--- client/pages/map.tsx
+++ client/pages/map.tsx
@@ -23,14 +23,43 @@ const MapSection = ({
 	playerId
 }: MapSectionProps) => {
 	const sectionHeightWidth = 10;
+	const id = `${hasPlayer || hasPlayerCharacter ? playerId : 'section'}-${sectionId}`
+	const playerBackgroundColor = hasPlayerCharacter ? '#ff0000' : hasPlayer ? '#0000ff' : section.hexColor;
+	const adjustColor = (color, amount) => {
+		return '#' + color.replace(/^#/, '').replace(/../g, color => ('0' + Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+	}
 	return (
 		<div
-			key={`${hasPlayer || hasPlayerCharacter ? playerId : 'section'}-${sectionId}`}
+			key={id}
 			style={{
-				backgroundColor: hasPlayerCharacter ? '#ff0000' : hasPlayer ? '#0000ff' : section.hexColor,
+				backgroundColor: section.hexColor,
 				height: `${sectionHeightWidth}px`,
-				width: `${sectionHeightWidth}px`
-			}}></div>
+				width: `${sectionHeightWidth}px`,
+				display: "grid",
+				justifyContent: "center",
+				alignItems: "center"
+			}}>
+			{
+				hasPlayerCharacter || hasPlayer ?
+					(
+						<div id={id} style={{
+							backgroundColor: adjustColor(playerBackgroundColor, -10),
+							height: `${sectionHeightWidth / 1.3}px`,
+							width: `${sectionHeightWidth / 1.3}px`,
+							borderRadius: '10px'
+						}}></div>
+					)
+					:
+					(
+						<div id={id} style={{
+							backgroundColor: adjustColor(section.hexColor, 10),
+							height: `${sectionHeightWidth / 1.2}px`,
+							width: `${sectionHeightWidth / 1.2}px`
+						}}></div>
+					)
+			}
+
+		</div>
 	)
 }
 const Map = (props: Props) => {
