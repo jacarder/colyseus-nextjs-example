bc7a6f25c6f9f8f00529dbe8ddd6abf2a820f79a
Jacarder <jonandrewcarder@gmail.com>
2022-05-07 00:47:14 -0400

    clean up and messing with max visible size

--- client/pages/map.tsx
+++ client/pages/map.tsx
@@ -3,12 +3,13 @@ import * as Colyseus from "colyseus.js"
 import { usePlayerListeners } from "../hooks/usePlayerListeners";
 import { Room } from "colyseus.js";
 import MapSection from "../components/MapSection/MapSection";
+import { IMapSectionInfo } from "../models/mapSection.model";
 
 type Props = {}
 
 const client = new Colyseus.Client('ws://localhost:2567');
 const Map = (props: Props) => {
-
+	const maxSize: { x: number, y: number } = { x: 20, y: 20 }
 	const [mapData, setMapData] = useState<{ grid: [][] }>({ grid: [] })
 	const [room, setRoom] = useState<Room<unknown>>();
 	const [playerCharacter, players] = usePlayerListeners(room)
@@ -38,29 +39,54 @@ const Map = (props: Props) => {
 	const getPlayerIdByRowSection = (rowIndex: number, sectionIndex: number) => {
 		return Object.keys(players || {}).find(key => players[key].location.x === rowIndex && players[key].location.y === sectionIndex)
 	}
+	const generateMapSection = (rowIndex: number, sectionIndex: number, section: IMapSectionInfo) => {
+		if (
+			rowIndex === playerCharacter?.location?.x
+			&&
+			sectionIndex === playerCharacter?.location?.y) {
+			return (
+				<MapSection
+					key={`${rowIndex}-${sectionIndex}-test`}
+					sectionId={`${rowIndex}-${sectionIndex}`}
+					section={section}
+					hasPlayerCharacter
+					playerId={getPlayerIdByRowSection(rowIndex, sectionIndex)}
+				/>
+			)
+		} else if (isPlayerLocated(rowIndex, sectionIndex)) {
+			return (
+				<MapSection
+					sectionId={`${rowIndex}-${sectionIndex}`}
+					section={section}
+					hasPlayer
+					playerId={getPlayerIdByRowSection(rowIndex, sectionIndex)}
+				/>
+			)
+		} else {
+			return (
+				<MapSection sectionId={`${rowIndex}-${sectionIndex}`} section={section} />
+			)
+		}
+	}
 	return (
 		<div id="mapid">
 			{Object.keys(players || {}).length || 1}
 			{mapData.grid.map((row: any, rowIndex: number) =>
 				<div key={`row-${rowIndex}`} style={{ display: 'flex' }}>
-					{row.map((section: any, sectionIndex: number) =>
-						<>
-							{
-								rowIndex === playerCharacter?.location?.x && sectionIndex === playerCharacter?.location?.y ?
-									(
-										<MapSection key={`${rowIndex}-${sectionIndex}-test`} sectionId={`${rowIndex}-${sectionIndex}`} section={section} hasPlayerCharacter playerId={getPlayerIdByRowSection(rowIndex, sectionIndex)} />
-									)
-									: isPlayerLocated(rowIndex, sectionIndex) ?
-										(
-											<MapSection sectionId={`${rowIndex}-${sectionIndex}`} section={section} hasPlayer playerId={getPlayerIdByRowSection(rowIndex, sectionIndex)} />
-										)
-										:
-										(
-											<MapSection sectionId={`${rowIndex}-${sectionIndex}`} section={section} />
-										)
-							}
-						</>
-					)}
+					{row.map((section: any, sectionIndex: number) => {
+						if (
+							(
+								rowIndex <= (playerCharacter.location.x + maxSize.x) &&
+								rowIndex >= (playerCharacter.location.x - maxSize.x)
+							) &&
+							(
+								sectionIndex >= (playerCharacter.location.y - maxSize.y) &&
+								sectionIndex <= (playerCharacter.location.y + maxSize.y)
+							)
+						) {
+							return generateMapSection(rowIndex, sectionIndex, section);
+						}
+					})}
 				</div>
 			)
 			}
